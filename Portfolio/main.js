bf9bbe6cc808f0747b585623698158f843946519
carson <carsonclark2009@gmail.com>
2021-08-07 00:07:46 -0300

    added code that would ensure ssettings saved in localStorage

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -4,9 +4,11 @@ const toggleBtn = document.querySelector("input[type=checkbox]");
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }
 }
 
