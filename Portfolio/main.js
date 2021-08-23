fddcefcca4aa236e8085d6f00a057711e245014c
ClarkTheCoder <carsonclark2009@gmail.com>
2021-08-23 12:26:30 -0500

    added dark/light icon to toggle button

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,16 +1,21 @@
 
 const toggleBtn = document.querySelector("input[type=checkbox]");
+const themeIcon = document.querySelector("#theme-icon")
 
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        localStorage.setItem('theme', 'dark')
+        themeIcon.innerHTML = '<i class="fas fa-sun fa-2x"></i>'
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        themeIcon.innerHTML = '<i class="fas fa-moon fa-2x"></i>'
+
     }
 }
 
 toggleBtn.addEventListener('change', switchTheme, false);
+
 
